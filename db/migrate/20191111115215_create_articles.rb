729b457b984e7735e5ef95979165fff701a0ac4b
Rachael <rachael.coulter@kairostech.io>
2019-11-20 15:48:38 +0000

    Can delete articles

--- db/migrate/20191111115215_create_articles.rb
+++ db/migrate/20191111115215_create_articles.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :body
+      t.string(:title)
+      t.text(:body)
       t.timestamps
     end
   end
